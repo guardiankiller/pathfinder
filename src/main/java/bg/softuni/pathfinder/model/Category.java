568ebc026edd008195d8d265242d2ed6f93e5b78
GuardianKiller <com_1998@abv.bg>
2024-05-31 01:32:36 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/pathfinder/model/Category.java
@@ -0,0 +1,47 @@
+package bg.softuni.pathfinder.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CategoryNames name;
+
+    public Category() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CategoryNames getName() {
+        return name;
+    }
+
+    public void setName(CategoryNames name) {
+        this.name = name;
+    }
+}
